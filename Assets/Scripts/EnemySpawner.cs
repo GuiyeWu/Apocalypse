87c4828db67aa51fa13a891f96edfa7fa2ba685a
unknown <wug13@mcmaster.ca>
2019-04-27 20:21:48 -0400

    Apocalypse

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public List<GameObject> Enemies = new List<GameObject>();
+    public static float spawnRate;
+    public static int NumberOfEnemies;
+
+    private float x, y;
+    private int counting;
+    private Vector3 spawnPos;
+
+    void Start()
+    {
+        NumberOfEnemies = 5;
+        spawnRate = 2;
+        counting = 0;
+        StartCoroutine(SpawnTestEnemy());
+    }
+
+    public IEnumerator SpawnTestEnemy()
+    {
+        x = Random.Range(-5, 5);
+        y = Random.Range(-5, 5);
+        spawnPos.x += x;
+        spawnPos.y += y;
+        Instantiate(Enemies[0], spawnPos, Quaternion.identity);
+        counting += 1;
+        yield return new WaitForSeconds(spawnRate);
+        if (counting < NumberOfEnemies)
+        {
+            StartCoroutine(SpawnTestEnemy());
+        } else
+        {
+            while(counting >= NumberOfEnemies)
+            {
+                yield return new WaitForSeconds(1);
+            }
+            StartCoroutine(SpawnTestEnemy());
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStats : MonoBehaviour
+{
+    public static PlayerStats playerStats;
+
+    public GameObject player;
+    public Text healthText;
+    public Slider healthSlider;
+
+    public float health;
+    public float maxHealth;
+
+    public int level;
+
+    public int coins;
+    public int gems;
+
+    public Text coinsValue;
+    public Text gemsValue;
+
+    public GameObject GameOverUI;
+
+    void Awake()
+    {
+        /*if (playerStats != null)
+        {
+            Destroy(playerStats);
+        }
+        else
+        {*/
+            playerStats = this;
+        //}
+        DontDestroyOnLoad(this);
+    }
+
+    void Start()
+    {
+        health = maxHealth;
+        level = 1;
+        SetHealUI();
+        GameOverUI.SetActive(false);
+    }
+    public void DealDamage(float damage)
+    {
+        health -= damage;
+        CheckDeath();
+        SetHealUI();
+    }
+
+    public void HealCharacter(float heal)
+    {
+        health += heal;
+        CheckOverheal();
+        SetHealUI();
+    }
+
+    private void SetHealUI()
+    {
+        healthSlider.value = CalculateHealthPercentage();
+        healthText.text = Mathf.Ceil(health).ToString() + " / " + Mathf.Ceil(maxHealth).ToString();
+    }
+
+    private void CheckOverheal()
+    {
+        if (health > maxHealth)
+        {
+            health = maxHealth;
+        }
+    }
+    private void CheckDeath()
+    {
+        if (health <= 0)
+        {
+            health = 0;
+            GameOverUI.SetActive(true);
+            Time.timeScale = 0;
+            //Destroy(player);
+        }
+    }
+
+    float CalculateHealthPercentage()
+    {
+        return health / maxHealth;
+    }
+
+    public void AddCurrency(CurrenctPickUp currency)
+    {
+        if(currency.currentObject == CurrenctPickUp.PickUpObject.COIN)
+        {
+            coins += currency.pickupQuantity;
+            coinsValue.text = "Gold: " + coins.ToString();
+        }
+        else if(currency.currentObject == CurrenctPickUp.PickUpObject.GEM)
+        {
+            gems += currency.pickupQuantity;
+            gemsValue.text = "Gems " + gems.ToString();
+        }
+    }
+}
